6475fef54db9928738851bd24cf87a211c74f1e0
CI automation <ci@rstream.io>
2025-05-30 15:00:01 +0000

    update spawnai-runner cask to 1.0.1

--- Casks/spawnai-runner.rb
+++ Casks/spawnai-runner.rb
@@ -9,21 +9,21 @@ cask "spawnai-runner" do
     "macos" => {
       "x86_64" => {
         url: "https://rstream.io/api/packages/6839c445bd339d43881ff42b/download",
-        sha256: "6066c2921a6ab7e5f84109b51b02df86e29c6c00183c4d69640f786ae46d4932",
+        sha256: "bfd050a46badd6a0c979f3242c841e23716f6721e3c02f6c29d7ca15a19bbf71",
       },
       "arm64" => {
         url: "https://rstream.io/api/packages/6839c444008a72be4945bd7a/download",
-        sha256: "f155e19d30158d010a36c82af01c1f0eb40196b85da412cb8217cca648b43576",
+        sha256: "9549f8bfefed5b1034ce138d77b3856c6b98f64ae251269181386d3b37088093",
       }
     },
     "linux" => {
       "x86_64" => {
         url: "https://rstream.io/api/packages/6839c4468fb4e073e6351acf/download",
-        sha256: "23dd00fdc3dc481972fb6ec9640679151e571fb906b560e6732142e0c6f6d4cf",
+        sha256: "8d6ca439a295ad26b6a843627b44b76d77cf0c7940526a3ed9841857557b6885",
       },
       "arm64" => {
         url: "https://rstream.io/api/packages/6839c447bda48d61d2f4a776/download",
-        sha256: "41ce1f4e7e8ffe0dc4c2d511da0a2c570db2b31fba5a8bc5b28400b61facc0aa",
+        sha256: "e9b066b0a43d805cbb2da2b1d7084566c7e09277b1d48e351e4534f3a1246ad3",
       }
     }
   }
