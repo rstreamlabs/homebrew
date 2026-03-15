9b58b1522c683f31e03b6d3c85e0f67e2e7a6d5b
CI automation <ci@rstream.io>
2026-03-15 11:09:49 +0000

    update rstream formula to 1.7.2

--- Formula/rstream.rb
+++ Formula/rstream.rb
@@ -1,23 +1,23 @@
 class Rstream < Formula
   desc "Serverless networking."
   homepage "https://rstream.io"
-  version "1.7.1"
+  version "1.7.2"
   on_macos do
     if Hardware::CPU.intel?
-      url "https://rstream.io/api/packages/cmmmjdak6000004jrfgybghyd/download"
-      sha256 "c8a7a3c354dd53912bf4e06db9419f9f5f32d2e7a20fd99ba48851ef37767f7c"
+      url "https://rstream.io/api/packages/cmmrnj6pi000004l1tm8rucc7/download"
+      sha256 "8ab1ed0959bf5e1977859f09baf0ef1b2cc2206073ffb5909fa85f0a58419696"
     else
-      url "https://rstream.io/api/packages/cmmmjd8fv000104joc06rlfhv/download"
-      sha256 "d5d1b1b521110e073a201d5f47606af34729292a3223cb628fad515d0bc58c0b"
+      url "https://rstream.io/api/packages/cmmrnj6vg000004jv3275slfw/download"
+      sha256 "ed631bed600028b3790001d56c5778115aa4fd0768d04107492c9ad54d3fb0af"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://rstream.io/api/packages/cmmmjdb0e000c04jozwiuvll7/download"
-      sha256 "5d37cae05e0420a85039fbbcba4f185cf821f371ac0dbbb0524dd5feb7a650c7"
+      url "https://rstream.io/api/packages/cmmrnjbkd000404jyz67ftsfx/download"
+      sha256 "62c6e1bb65c36fb34ffb9e00a8ced33bc56382b7320fcd87b4d299d8c29ab897"
     else
-      url "https://rstream.io/api/packages/cmmmjdc0b000204kwelxvv135/download"
-      sha256 "a232979c6ed1772a57cc615a925674e4e0cf0cec7b005f253499d72bf28ceee6"
+      url "https://rstream.io/api/packages/cmmrnjfjs000d04l2b31fkp7y/download"
+      sha256 "fd44b184c86f88c83b3905dfccc6af48e4a0b8b82c032cea82e69f0a0e51532a"
     end
   end
   def install
